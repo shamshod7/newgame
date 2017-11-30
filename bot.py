e983fbef7517d60b03e522d81140f98ef7598dcc
egor5q <31898567+egor5q@users.noreply.github.com>
2017-11-30 13:47:16 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,8 +7,7 @@ import opr_data
 import math
 import random
 import threading
-#from telebot import types
-types = telebot.types
+from telebot import types
 token = os.environ['TELEGRAM_TOKEN']
 bot = telebot.TeleBot(opr_config.token)
 
