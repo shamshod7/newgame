21bff6a38a577da4e8b49c7b315015bccb60c9a5
egor5q <31898567+egor5q@users.noreply.github.com>
2017-12-05 11:44:16 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import redis
+import os
+import telebot
+import math
+import random
+import threading
+from telebot import types
+token = os.environ['TELEGRAM_TOKEN']
+
+bot = telebot.TeleBot(token)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
