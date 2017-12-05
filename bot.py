f9c850370eb37fa7840fb500862ad32cdb575822
egor5q <31898567+egor5q@users.noreply.github.com>
2017-12-05 12:52:39 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,6 +12,17 @@ bot = telebot.TeleBot(token)
 
 
 
+@bot.message_handler(commands=['start'])
+def startmessage(message):
+  m=message.from_user.id
+  bot.send_message(m, 'Приветствую тебя в игре "MagicWars! Здесь тебе предстоит научиться создавать свои мистические войска и сражаться с друзьями'+
+                  ', а так же изучать особенности каждого своего воина! Жми /help, чтобы узнать всё в подробностях') 
+                  
+                   
+
+
+
+
 
 
 
