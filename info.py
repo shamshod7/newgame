0189f451f8594f9d667bddc0cb8b3055091eed47
egor5q <31898567+egor5q@users.noreply.github.com>
2017-12-06 21:34:44 +0300

    Update info.py

--- info.py
+++ info.py
@@ -3,4 +3,21 @@ class Lobby(object):
         self.game={}
         
         
+class S_me4nik(object):
+    def __init__(self):
+        self.hp=120
+        self.mana=0
+        self.damage=50
+        self.cost=100
+        self.type='dead'
+        self.fromelectrodmg=0.8
+        self.frombiodmg=0.8
+        self.fromghostdmg=0.8
+        self.fromdeaddmg=0.8
+        
+        
+
+        
+        
+s_me4nik=S_me4nik()        
 lobby = Lobby()
