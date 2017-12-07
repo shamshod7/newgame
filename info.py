8020137945d878e5dca3b1784a75860bfdd19187
egor5q <31898567+egor5q@users.noreply.github.com>
2017-12-07 23:13:08 +0300

    Update info.py

--- info.py
+++ info.py
@@ -15,6 +15,7 @@ class S_me4nik(object):
         self.frombiodmg=0.8
         self.fromghostdmg=1.1
         self.fromdeaddmg=0.8
+        self.fromfiredmg=1.1
         
         
 class Pyos(object):
@@ -29,12 +30,42 @@ class Pyos(object):
         self.frombiodmg=1.0
         self.fromghostdmg=0.85
         self.fromdeaddmg=1.0
+        self.fromfiredmg=1.0
+        
+class Phoenix(object):
+    def __init__(self):
+        self.name='Фениксадзе'
+        self.hp=1
+        self.mana=0
+        self.damage=330
+        self.cost=200
+        self.type='fire'
+        self.fromelectrodmg=1.0
+        self.frombiodmg=1.0
+        self.fromghostdmg=1.0
+        self.fromdeaddmg=1.0
+        self.fromfiredmg=1.0
+        
+        
+class Electromagnit(object):
+    def __init__(self):
+        self.name='Электромагнитень'
+        self.hp=200
+        self.mana=100
+        self.damage=125
+        self.cost=200
+        self.type='electro'
+        self.fromelectrodmg=1.0
+        self.frombiodmg=1.0
+        self.fromghostdmg=1.2
+        self.fromdeaddmg=1.0
+        self.fromfiredmg=1.0
         
 
         
         
-
-        
+electromagnit=Electromagnit()
+phoenix=Phoenix()        
 pyos=Pyos()       
 s_me4nik=S_me4nik()        
 lobby = Lobby()
