523a9650921e5e407521423b0ee43412a395e113
egor5q <31898567+egor5q@users.noreply.github.com>
2017-12-07 22:47:46 +0300

    Update info.py

--- info.py
+++ info.py
@@ -13,12 +13,28 @@ class S_me4nik(object):
         self.type='dead'
         self.fromelectrodmg=0.8
         self.frombiodmg=0.8
-        self.fromghostdmg=0.8
+        self.fromghostdmg=1.1
         self.fromdeaddmg=0.8
         
+        
+class Pyos(object):
+    def __init__(self):
+        self.name='Pyos'
+        self.hp=200
+        self.mana=50
+        self.damage=100
+        self.cost=175
+        self.type='bio'
+        self.fromelectrodmg=1.2
+        self.frombiodmg=1.0
+        self.fromghostdmg=0.85
+        self.fromdeaddmg=1.0
         
 
         
         
+
+        
+pyos=Pyos()       
 s_me4nik=S_me4nik()        
 lobby = Lobby()
