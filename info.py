20ce09b4b0b0724b474f62f588e63b2b60ca3bdc
egor5q <31898567+egor5q@users.noreply.github.com>
2017-12-09 11:44:53 +0300

    Update info.py

--- info.py
+++ info.py
@@ -9,12 +9,12 @@ class S_me4nik(object):
         self.hp=120
         self.mana=0
         self.damage=50
-        self.cost=25
+        self.cost=70
         self.type='dead'
-        self.fromelectrodmg=0.8
-        self.frombiodmg=0.8
+        self.fromelectrodmg=0.7
+        self.frombiodmg=1.5
         self.fromghostdmg=1.1
-        self.fromdeaddmg=0.8
+        self.fromdeaddmg=1.0
         self.fromfiredmg=1.1
         
         
@@ -24,44 +24,57 @@ class Pyos(object):
         self.hp=200
         self.mana=50
         self.damage=100
-        self.cost=175
+        self.cost=130
         self.type='bio'
-        self.fromelectrodmg=1.2
+        self.fromelectrodmg=1.3
         self.frombiodmg=1.0
-        self.fromghostdmg=0.85
-        self.fromdeaddmg=1.0
-        self.fromfiredmg=1.0
+        self.fromghostdmg=0.8
+        self.fromdeaddmg=1.1
+        self.fromfiredmg=1.5
         
 class Phoenix(object):
     def __init__(self):
-        self.name='Фениксадзе'
+        self.name='Феникадзе'
         self.hp=1
         self.mana=0
         self.damage=330
         self.cost=200
         self.type='fire'
-        self.fromelectrodmg=1.0
-        self.frombiodmg=1.0
-        self.fromghostdmg=1.0
-        self.fromdeaddmg=1.0
+        self.fromelectrodmg=0.5
+        self.frombiodmg=0.7
+        self.fromghostdmg=1.5
+        self.fromdeaddmg=1.2
         self.fromfiredmg=1.0
         
         
 class Electromagnit(object):
     def __init__(self):
         self.name='Электромагнитень'
-        self.hp=200
+        self.hp=190
         self.mana=100
-        self.damage=125
-        self.cost=200
+        self.damage=105
+        self.cost=175
         self.type='electro'
         self.fromelectrodmg=1.0
-        self.frombiodmg=1.0
-        self.fromghostdmg=1.2
-        self.fromdeaddmg=1.0
-        self.fromfiredmg=1.0
+        self.frombiodmg=1.1
+        self.fromghostdmg=0.8
+        self.fromdeaddmg=1.5
+        self.fromfiredmg=0.5
         
-
+        
+class Manoed(object):
+    def __init__(self):
+        self.name='Маноед'
+        self.hp=85
+        self.mana=0
+        self.damage=70
+        self.cost=165
+        self.type='ghost'
+        self.fromelectrodmg=1.5
+        self.frombiodmg=0.75
+        self.fromghostdmg=1.0
+        self.fromdeaddmg=0.75
+        self.fromfiredmg=0.9
         
         
 electromagnit=Electromagnit()
