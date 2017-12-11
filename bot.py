b7e240c18b80b425d6964c061029936cf383a910
egor5q <31898567+egor5q@users.noreply.github.com>
2017-12-12 00:13:56 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -74,8 +74,10 @@ def mobdmg(mob, creatorid, team, team2, number):
                         info.lobby.game[creatorid][team][mob][number]['maxkoef']=info.lobby.game[creatorid][team][mob][number]['koef']
                         info.lobby.game[creatorid][team][mob][number]['target']=info.lobby.game[creatorid][team2][mob2][number2]
                         t=info.lobby.game[creatorid][team2][mob2][number2]
-                        if t['hp']-(info.lobby.game[creatorid][team][mob][number]['damage']*info.lobby.game[creatorid][team][mob][number]['maxkoef'])<1:
+                        xx=t['hp']-(info.lobby.game[creatorid][team][mob][number]['damage']*info.lobby.game[creatorid][team][mob][number]['maxkoef'])
+                        if xx<1:
                             t['potentialdie']=1
+                        print(t['potentialdie'])
                             
 
     if t==None:
