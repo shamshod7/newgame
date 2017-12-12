a8f93c5b3a79953e96035eded3a44f7ba1b85c75
egor5q <31898567+egor5q@users.noreply.github.com>
2017-12-12 18:45:23 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -250,10 +250,10 @@ def skills(mob, creatorid, team, team2):
                   emojdie=emojize(':x:', use_aliases=True)
                   emojdmg=emojize(':broken_heart:', use_aliases=True)
                   emojhp=emojize(':green_heart:', use_aliases=True)
-                  if team='t1mobs':
+                  if team=='t1mobs':
                     info.lobby.game[creatorid]['thronet2hp']-=50
                     info.lobby.game[creatorid][team][mob][number]['skilltext']=emoj1+'Pyos'+emojattack+'Крепость'+emojdmg+'50.0'+emojhp+str(info.lobby.game[creatorid]['thronet2hp'])
-                  elif team='t2mobs':
+                  elif team=='t2mobs':
                     info.lobby.game[creatorid]['thronet1hp']-=50
                     info.lobby.game[creatorid][team][mob][number]['skilltext']=emoj1+'Pyos'+emojattack+'Крепость'+emojdmg+'50.0'+emojhp+str(info.lobby.game[creatorid]['thronet1hp'])
                 else:
