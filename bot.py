99a12c6ae97ca616451eb340b4c7a8358b1e4636
egor5q <31898567+egor5q@users.noreply.github.com>
2017-12-13 17:22:47 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -318,15 +318,17 @@ def endturn(creatorid):
    for mob1 in info.lobby.game[creatorid]['t1mobs']:
         for number111 in info.lobby.game[creatorid]['t1mobs'][mob1]:
            if info.lobby.game[creatorid]['t1hod']==1:
+            if info.lobby.game[creatorid]['t1mobs'][mob1][number111]['ready']!=1:
              skills(mob1, creatorid, 't1mobs', 't2mobs', number111)
              info.lobby.game[creatorid]['t1hod']=0
-             j=0       
+             e=0       
    for mob2 in info.lobby.game[creatorid]['t2mobs']:
        for number222 in info.lobby.game[creatorid]['t2mobs'][mob2]:
          if info.lobby.game[creatorid]['t1hod']==0:
+          if info.lobby.game[creatorid]['t2mobs'][mob2][number222]['ready']!=1:
            skills(mob2, creatorid, 't2mobs', 't1mobs', number222)
            info.lobby.game[creatorid]['t1hod']=1
-           e=0
+           j=0
    for readymob1 in info.lobby.game[creatorid]['t1mobs']:
      for readynumber1 in info.lobby.game[creatorid]['t1mobs'][readymob1]:
        if info.lobby.game[creatorid]['t1mobs'][readymob1][readynumber1]['ready']==1:
