9e14f0cc806e58020180cd3cefbdfb4ef70fd758
egor5q <31898567+egor5q@users.noreply.github.com>
2017-12-13 17:05:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -307,24 +307,26 @@ def endturn(creatorid):
    readymobs=0
    if info.lobby.game[creatorid]['t1hod']==0:
             e+=1
+            print('1')
+            if e>3:
+              info.lobby.game[creatorid]['t1hod']=1
    elif info.lobby.game[creatorid]['t1hod']==1:
             j+=1
+            print('2')
+            if j>3:
+               info.lobby.game[creatorid]['t1hod']=0 
    for mob1 in info.lobby.game[creatorid]['t1mobs']:
         for number111 in info.lobby.game[creatorid]['t1mobs'][mob1]:
            if info.lobby.game[creatorid]['t1hod']==1:
              skills(mob1, creatorid, 't1mobs', 't2mobs', number111)
              info.lobby.game[creatorid]['t1hod']=0
              e=0       
-        if e>3:
-            info.lobby.game[creatorid]['t1hod']=1
    for mob2 in info.lobby.game[creatorid]['t2mobs']:
        for number222 in info.lobby.game[creatorid]['t2mobs'][mob2]:
          if info.lobby.game[creatorid]['t1hod']==0:
            skills(mob2, creatorid, 't2mobs', 't1mobs', number222)
            info.lobby.game[creatorid]['t1hod']=1
            j=0
-       if j>3:
-            info.lobby.game[creatorid]['t1hod']=0  
    for readymob1 in info.lobby.game[creatorid]['t1mobs']:
      for readynumber1 in info.lobby.game[creatorid]['t1mobs'][readymob1]:
        if info.lobby.game[creatorid]['t1mobs'][readymob1][readynumber1]['ready']==1:
