f0184d92dfc3d753d957305ef3cf48c28c9ef9ad
egor5q <31898567+egor5q@users.noreply.github.com>
2017-12-17 13:43:28 +0300

    Update info.py

--- info.py
+++ info.py
@@ -139,7 +139,7 @@ class Vsadnik(object):
         self.hp=200
         self.mana=50
         self.damage=50
-        self.cost=40
+        self.cost=45
         self.type='ghost'
         self.fromelectrodmg=1.5
         self.frombiodmg=0.75
