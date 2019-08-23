19298f0f0a412c50285ad0a90b9a1133074ea39c
shamshod7 <45027158+shamshod7@users.noreply.github.com>
2019-08-23 16:10:45 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1488,7 +1488,6 @@ def undead(message):
 
 @bot.message_handler(commands=['fight'])
 def fightstart(message):
-  if message.from_user.id in info.lobby.game:
     if message.chat.id==info.lobby.game[message.from_user.id]['chatid']:
       if info.lobby.game[message.from_user.id]['battle']==0:
         if len(info.lobby.game[message.from_user.id]['players'])%2==0:
@@ -1525,7 +1524,7 @@ def fightstart(message):
             bot.send_message(441399484, 'Игра началась в каком-то чате!')
             info.lobby.game[message.from_user.id]['thread']=btl
             print(info.lobby.game)
-            info.lobby.game[message.from_user.id]['playing']==0:
+            info.lobby.game[message.from_user.id]['playing']=1
           else:
             bot.send_message(message.chat.id, 'Игра ('+info.lobby.game[message.from_user.id]['name']+') уже была запущена!')
          else:
@@ -1566,7 +1565,6 @@ def joinm(message):
 
 @bot.message_handler(commands=['cancel'])
 def cancelmessage(message):
-  if message.from_user.id in info.lobby.game:
     if info.lobby.game[message.from_user.id]['playing']==0:
       cancel(message.from_user.id, message.chat.id)
     else:
@@ -1619,7 +1617,7 @@ def beginmessage(message):
       print(info.lobby.game)
       bot.send_message(message.chat.id, 'Лобби создано! Назовите его, отправив название следующим сообщением.'+"\n"+'Если вы хотите отменить игру - нажмите /cancel.'+"\n"+'Игра автоматически удалится через 5 минут!')
       info.lobby.game[message.from_user.id]['naming']=1
-      lobbycancel=threading.Timer(30.0, cancel, args=[message.from_user.id, message.chat.id])
+      lobbycancel=threading.Timer(300.0, cancel, args=[message.from_user.id, message.chat.id])
       lobbycancel.start()
         
    else:
