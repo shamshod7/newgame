e6907867d36ad29bb5b245c60ec043000b3102dd
shamshod7 <45027158+shamshod7@users.noreply.github.com>
2019-08-23 23:00:51 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1606,7 +1606,7 @@ def beginmessage(message):
       createdlobby=createlobby(message.chat.id, message.from_user.id, message.from_user.first_name)
       info.lobby.game.update(createdlobby)
       print(info.lobby.game)
-      bot.send_document(message.chat.id,test.ratid,caption='O`yin yaratildi! Keyingi jo`natadigan xatingiz bilan uni nomlang!'+"\n"+'Agarda siz o`yinni to`xtatmoqchi bo`lsaz  /cancel tugmasini bosing.'+"\n"+'O`yin 20 daqiqadan so`ng avtomatik o`chiriladi!')   
+      bot.send_document(message.chat.id,test.ratid,caption="*O`yin yaratildi! Keyingi jo`natadigan xatingiz bilan uni nomlang!*"+"\n"+"*Agarda siz o`yinni to`xtatmoqchi bo`lsaz* /cancel *tugmasini bosing.*"+"\n"+"*O`yin 20 daqiqadan so`ng avtomatik o`chiriladi!*", parse_mode='markdown')   
       info.lobby.game[message.from_user.id]['naming']=1
       lobbycancel=threading.Timer(1200.0, cancel, args=[message.from_user.id, message.chat.id])
       lobbycancel.start()
