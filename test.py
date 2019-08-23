d41c0ea5a4214086a3bd5f7a4976b7d8c7debb0f
shamshod7 <45027158+shamshod7@users.noreply.github.com>
2019-08-24 00:02:18 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import os
 
-ratid = 'https://i.gifer.com/M8GV.gif'
-newid = 'AgADAgAD5aoxG_lWeUnx8zvZyrnBQ4W_UQ8ABHdGzvOMj3Qu-9EFAAEC'
-terrorid = 'AgADAgAD5KoxG_lWeUkIaPiVI3KM2XZrUw8ABD8X9N3NFnoU388FAAEC'
-masterid = 'AgADAgADbaoxG2TrgUmByGwbRN8P2APVUQ8ABNT-_u0Ly3fZ-dAFAAEC'
+ratid = 'https://i.gifer.com/I3ZZ.gif' #begin
+newid = 'http://img1.joyreactor.cc/pics/post/Хоббит-Арда-фэндомы-Саурон-1745899.gif' #fight
+terrorid = 'https://66.media.tumblr.com/f493d98acfddfc4aceb7059de14de460/tumblr_pfmi6cj8fQ1rj78ymo1_1280.gif' #himoya
+masterid = 'https://66.media.tumblr.com/8f7f96896ad85530cdce514213114bb2/tumblr_pnp6og8Rqc1rk9c31o1_500.gif' #xujum
 dragoid = 'AgADAgADuqsxGxGXeUleegeyeXJOg2rVtw8ABGqhcwGCHkzc0ysBAAEC'
 dogid = 'AgADAgAD46oxG_lWeUlL8j-SqUY82u348Q4ABDRQTRVc9JvZBVUBAAEC'
 rhinoid = 'AgADAgADbqoxG2TrgUkhECUTDgcfAkRsUw8ABGx3RFITYKWUotoFAAEC'
