4d01a02e5d66f8c8954dd2846cf34f040aa2e724
shamshod7 <45027158+shamshod7@users.noreply.github.com>
2019-08-23 22:28:38 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
+import os
 
+ratid = 'https://i.gifer.com/M8GV.gif'
+newid = 'AgADAgAD5aoxG_lWeUnx8zvZyrnBQ4W_UQ8ABHdGzvOMj3Qu-9EFAAEC'
+terrorid = 'AgADAgAD5KoxG_lWeUkIaPiVI3KM2XZrUw8ABD8X9N3NFnoU388FAAEC'
+masterid = 'AgADAgADbaoxG2TrgUmByGwbRN8P2APVUQ8ABNT-_u0Ly3fZ-dAFAAEC'
+dragoid = 'AgADAgADuqsxGxGXeUleegeyeXJOg2rVtw8ABGqhcwGCHkzc0ysBAAEC'
+dogid = 'AgADAgAD46oxG_lWeUlL8j-SqUY82u348Q4ABDRQTRVc9JvZBVUBAAEC'
+rhinoid = 'AgADAgADbqoxG2TrgUkhECUTDgcfAkRsUw8ABGx3RFITYKWUotoFAAEC'
+gameid = 'AgADAgADuasxGxGXeUlDpLyfuS6uCYpVUw8ABIBjKH6iBtWX5M8FAAEC'
+supid = 'AgADAgADb6oxG2TrgUmcuN7VctFI-a5KUw8ABJibYbS7b2Ln59UFAAEC'
+cusid = 'AgADAgADvKsxGxGXeUmWXVHd5atXpMTCtw8ABAhIJUlfpdhKiC0BAAEC'
