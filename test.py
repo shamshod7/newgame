9d8c9d90e0ea36665535c3ee8a13eb547dac5015
shamshod7 <45027158+shamshod7@users.noreply.github.com>
2019-08-24 00:47:54 +0500

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ newid = 'http://img1.joyreactor.cc/pics/post/Хоббит-Арда-фэндом�
 terrorid = 'https://66.media.tumblr.com/f493d98acfddfc4aceb7059de14de460/tumblr_pfmi6cj8fQ1rj78ymo1_1280.gif' #himoya
 masterid = 'https://66.media.tumblr.com/8f7f96896ad85530cdce514213114bb2/tumblr_pnp6og8Rqc1rk9c31o1_500.gif' #xujum
 dragoid = 'https://vignette.wikia.nocookie.net/anime-characters-fight/images/c/cd/Медив07.gif'
-dogid = 'https://vignette.wikia.nocookie.net/anime-characters-fight/images/5/55/Медив03.gif'
+dogid = 'https://vignette.wikia.nocookie.net/anime-characters-fight/images/5/55/Медив03.gif/revision/latest?cb=20180401192844&path-prefix=ru'
 rhinoid = 'AgADAgADbqoxG2TrgUkhECUTDgcfAkRsUw8ABGx3RFITYKWUotoFAAEC'
 gameid = 'AgADAgADuasxGxGXeUlDpLyfuS6uCYpVUw8ABIBjKH6iBtWX5M8FAAEC'
 supid = 'AgADAgADb6oxG2TrgUmcuN7VctFI-a5KUw8ABJibYbS7b2Ln59UFAAEC'
